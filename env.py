4b8ce4d6e5c081bf62972959c07564eed325f995
AnasEljandi <anas.eljandi.110@gmail.com>
2025-09-18 22:32:16 +0100

    url add

--- env.py
+++ env.py
@@ -1,4 +1,5 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "<your-database-URL>")
+    "DATABASE_URL", "<postgresql://neondb_owner:npg_byV6tofrMSY1@ep-solitary-mountain-agocnhhs.c-2.eu-central-1.aws.neon.tech/rack_straw_lip_666641?sslmode=require>"
+)
